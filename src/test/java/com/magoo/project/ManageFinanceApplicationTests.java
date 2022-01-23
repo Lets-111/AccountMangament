4c5fe6e530bc9e76999cc60258ac50c488f1d46b
dipanshu <dipanshu.magoo96@gmail.com>
2022-01-23 13:07:41 +0530

    Initial comit

---
+++ src/test/java/com/magoo/project/ManageFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magoo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
